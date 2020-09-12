6c269dffe8711a69eb34c27ef06a997a7008251c
Michael Forman <mforman@gmail.com>
2020-09-12 21:35:50 +0100

    Reset tapping term to default value

--- keyboards/ergodox_ez/keymaps/mforman/config.h
+++ keyboards/ergodox_ez/keymaps/mforman/config.h
@@ -3,7 +3,9 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 #define TAPPING_TOGGLE  1
+
 #undef TAPPING_TERM
-#define TAPPING_TERM    150
+#define TAPPING_TERM 200  
+
 #define IGNORE_MOD_TAP_INTERRUPT
 
