da49823720ed6d7054b3c7c87501c506f0084d0a
Michael Forman <michael.forman@oaknorth.ai>
2020-09-14 09:49:54 +0100

    enable number row on ergodox

--- keyboards/ergodox_ez/keymaps/mforman/keymap.c
+++ keyboards/ergodox_ez/keymaps/mforman/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
 [_QWERTY] = LAYOUT_ergodox_pretty(
-_______, _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______,
+_______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , _______,      _______, KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______,
 _______, KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , _______,      _______, KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , _______,
 _______, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                        KC_H   , KC_J   , KC_K   , KC_L   , KC_QUOT, _______,
 _______, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , _______,      _______, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, _______,
