cdf630627717baab9eed0125c20c622e9cce7abc
Michael Forman <michael.forman@oaknorth.ai>
2020-09-20 18:51:25 +0100

    intial build for corne

--- keyboards/crkbd/keymaps/mforman/rules.mk
+++ keyboards/crkbd/keymaps/mforman/rules.mk
@@ -3,4 +3,6 @@ RGB_MATRIX_ENABLE  = WS2812
 MOUSEKEY_ENABLE    = no
 NKRO_ENABLE        = yes
 OLED_DRIVER_ENABLE = yes
-
+CONSOLE_ENABLE     = no     # Console for debug(+400)
+AUDIO_ENABLE       = no     # Audio output on port C6
+LINK_TIME_OPTIMIZATION_ENABLE = yes
